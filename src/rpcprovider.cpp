9cbf4c90db68fc1ba0e19514c2fb155db88fb8c8
MizarJiang <475453013@qq.com>
2023-04-29 16:02:09 +0800

    add file

--- src/rpcprovider.cpp
+++ src/rpcprovider.cpp
@@ -1,5 +1,6 @@
 #include "rpcprovider.h"
 #include "mprpcapplication.h"
+#include "rpcheader.pb.h"
 
 void RpcProvider::NotifyService(google::protobuf::Service *service)
 {
@@ -48,13 +49,63 @@ void RpcProvider::Run()
     server.start();
     _eventLoop.loop();
 }
-
+// 新的socket连接回调
 void RpcProvider::onConnetction(const muduo::net::TcpConnectionPtr &conn)
 {
+    if (!conn->connected())
+    {
+        // 断开连接
+        conn->shutdown();
+    }
 }
+/*
+在框架内部，RpcProvider和RpcConsumer协商好之间通信用的protobuf数据类型
+service_name method_name args
+定义proto的message类型，进行数据头的序列化和反序列化
+header_size(4字节)+header_str+args_str
+为了避免粘包问题，储存一个args_size
 
+使用std::string insert和copy方法，固定存储4个字节
+*/
+// 如果远程有一个rpc服务的调用请求，那么onMessage方法就会响应
 void RpcProvider::onMessage(const muduo::net::TcpConnectionPtr &conn,
                             muduo::net::Buffer *buff,
                             muduo::Timestamp time)
 {
+    // 网络上接收的远程rpc调用请求的字符流 Login args
+    std::string recv_buf = buff->retrieveAllAsString();
+
+    // 从字符流中读取前四个字节的内容
+    uint32_t header_size = 0;
+    recv_buf.copy((char *)&header_size, 4, 0);
+
+    // 根据header_size读取数据头的原始字符流，反序列化数据，得到rpc请求的详细信息
+    std::string rpc_header_str = recv_buf.substr(4, header_size);
+    mprpc::RpcHeader rpcHeader;
+    std::string service_name;
+    std::string method_name;
+    uint32_t args_size;
+    if (rpcHeader.ParseFromString(rpc_header_str))
+    {
+        // 数据头反序列化成功
+        service_name = rpcHeader.service_name();
+        method_name = rpcHeader.method_name();
+        args_size = rpcHeader.args_size();
+    }
+    else
+    {
+        // 数据头反序列化失败
+        std::cout << "rpc_header_str:" << rpc_header_str << " parase error!" << std::endl;
+        return;
+    }
+    // 获取rpc方法参数的字符流数据
+    std::string args_str = rpc_header_str.substr(4 + header_size, args_size);
+    // 打印调试信息
+    std::cout << "======================" << std::endl;
+    std::cout << "header_size: " << header_size << std::endl;
+    std::cout << "rpc_header_str: " << rpc_header_str << std::endl;
+    std::cout << "service_name: " << service_name << std::endl;
+    std::cout << "method_name: " << method_name << std::endl;
+    std::cout << "args_str: " << args_str << std::endl;
+    std::cout << "======================" << std::endl;
 }
